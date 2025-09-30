921d07494a659dc7e64499ddadbbaed917a65344
uratabhi <waitabhi2015@gmail.com>
2025-09-30 10:51:13 +0530

    feat: added project lombok dependency

--- src/main/java/com/abhishekjaiswal/journalApp/entity/JournalEntry.java
+++ src/main/java/com/abhishekjaiswal/journalApp/entity/JournalEntry.java
@@ -1,51 +1,22 @@
 package com.abhishekjaiswal.journalApp.entity;
 
+import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 import org.bson.types.ObjectId;
 import org.springframework.data.annotation.Id;
 import org.springframework.data.mongodb.core.mapping.Document;
 
-import javax.xml.crypto.Data;
 import java.time.LocalDateTime;
 
 @Document(collection = "journal_entries")
+@Data
 public class JournalEntry {
 
     @Id
     private ObjectId id;
 
-    public LocalDateTime getDate() {
-        return date;
-    }
-
-    public void setDate(LocalDateTime date) {
-        this.date = date;
-    }
-
     private LocalDateTime date;
-
-    public ObjectId getId() {
-        return id;
-    }
-
-    public void setId(ObjectId id) {
-        this.id = id;
-    }
-
-    public String getTitle() {
-        return title;
-    }
-
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    public String getContent() {
-        return content;
-    }
-
-    public void setContent(String content) {
-        this.content = content;
-    }
 
     private  String title;
 
